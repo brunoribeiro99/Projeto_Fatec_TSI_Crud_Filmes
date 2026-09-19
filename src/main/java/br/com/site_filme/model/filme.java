dcd94b8e587f89ef01ec40841732f66685a79227
Bruno Ribeiro <bhribeiro99@gmail.com>
2026-09-18 22:12:27 -0300

    Gerando a classe model

--- src/main/java/br/com/site_filme/model/filme.java
+++ src/main/java/br/com/site_filme/model/filme.java
@@ -2,4 +2,15 @@ package br.com.site_filme.model;
 
 public class filme {
 
+    private int id;
+    private String nome;
+    private int anoLancamento;
+    private String descricao;
+    private String genero;
+
+    public filme() {
+        
+    }
+    
+
 }
