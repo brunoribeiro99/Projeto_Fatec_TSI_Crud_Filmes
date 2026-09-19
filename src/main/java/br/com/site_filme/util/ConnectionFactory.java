17e7e2fb159d2f83fbdce49ab17c6256a3b2f896
Bruno Ribeiro <bhribeiro99@gmail.com>
2026-09-18 21:53:21 -0300

    Inserindo codigo no arq Connection Factory no pacote util

--- src/main/java/br/com/site_filme/util/ConnectionFactory.java
+++ src/main/java/br/com/site_filme/util/ConnectionFactory.java
@@ -1,5 +1,56 @@
 package br.com.site_filme.util;
 
-public class ConnectionFactory {
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Bruno Ribeiro
+ */
+public class ConnectionFactory {
+    public static Connection getConnection() throws Exception {
+        try {
+            Class.forName("org.postgresql.Driver");
+            return DriverManager.getConnection("jdbc:postgresql://localhost:5432/site_filme", "postgres", "123456");
+        } catch (Exception ex) {
+            throw new Exception(ex.getMessage());
+        }
+    }
+
+    public static void closeConnection(Connection conn, Statement stmt, ResultSet rs) throws Exception {
+        close(conn, stmt, rs);
+    }
+
+    public static void closeConnection(Connection conn, Statement stmt) throws Exception {
+        close(conn, stmt, null);
+    }
+
+    public static void closeConnection(Connection conn) throws Exception {
+        close(conn, null, null);
+    }
+
+    private static void close(Connection conn, Statement stmt, ResultSet rs) throws Exception {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+            if (stmt != null) {
+                stmt.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (Exception ex) {
+            throw new Exception(ex.getMessage());
+        }
+    }
+
+    public static void close(Connection conn, PreparedStatement stmt) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
 }
