4ba09b1567c50d8ade8df795e73b77a630c3365c
Bruno Henrique Ribeiro <bhribeiro99@gmail.com>
2026-09-25 15:57:00 -0300

    Criando os gets e sets

--- src/main/java/br/com/site_filme/model/filme.java
+++ src/main/java/br/com/site_filme/model/filme.java
@@ -1,6 +1,6 @@
 package br.com.site_filme.model;
 
-public class filme {
+public class Filme {
 
     private int id;
     private String nome;
@@ -8,8 +8,48 @@ public class filme {
     private String descricao;
     private String genero;
 
-    public filme() {
+    public Filme() {
         
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public void setAnoLancamento(int anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
     }
     
 
